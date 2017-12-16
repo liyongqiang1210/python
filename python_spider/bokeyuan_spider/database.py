d52040f78eaa5a16fa5db5a42fe61faa3e8e6470
liyongqiang <liyongqiang@DESKTOP-E4NHH8S>
2017-12-16 17:37:20 +0800

    更新sql

--- python_spider/bokeyuan_spider/database.py
+++ python_spider/bokeyuan_spider/database.py
@@ -15,50 +15,58 @@ import pymysql
 # 数据持久化类
 class Database(object):
 
-    # 初始化变量
-    def __init__(self):
-        # 打开数据连接
-        self.conn = pymysql.connect(
-            host='39.106.154.2', user='root', password='root', database='maven')
-        # 使用cursor()方法创建一个游标对象cur
-        self.cur = self.conn.cursor()
-
     # 插入url的方法
     def insertUrl(self, url, url_type, url_title):
         # 插入操作
-        insert_sql = 'INSERT INTO bokeyuan_url_list(url,url_type,url_title,is_craw)VALUES(%s,%s,%s,0)'
-        try:
-            while self.selectUrl(url):
-                self.cur.execute(insert_sql, [url_type, url_title])
-                self.conn.commit()
-                print(url + '-' * 10 + '插入成功')
-        except:
-            self.conn.rollback()
-            print(url + '-' * 10 + '插入失败')
+        insert_sql = "INSERT INTO bokeyuan_url_list(url,url_type,url_title,is_craw)VALUES('%s','%s','%s',0)" % (url, url_type, url_title)
+        while self.selectUrl(url):
+            try:
+                conn = pymysql.connect(host='39.106.154.2', user='root', password='root', database='maven')
+                cur = conn.cursor()
+                cur.execute(insert_sql)
+                conn.commit()
+                print(url + '----------插入成功')
+            except:
+                conn.rollback()
+                print(url + '----------已存在')
 
-        # 关闭查询
-        self.cur.close()
-
-        # 关闭数据库连接
-        self.conn.close()
+            # 关闭查询
+            cur.close()
+            
+            # 关闭数据库连接
+            conn.close()                       
 
     # 查询数据库是否存在此url
     def selectUrl(self, url):
+        conn = pymysql.connect(host='39.106.154.2', user='root', password='root', database='maven')
+        cur = conn.cursor()
         # 查询数据库是否存在当条数据
-        select_sql = 'SELECT COUNT(ID) FROM bokeyuan_url_list WHERE url = %s' % (url)
+        select_sql = "SELECT COUNT(ID) FROM bokeyuan_url_list WHERE url = '%s'" % (url)
         # 执行查询sql查询数据库是否存在此条数据
-        self.cur.execute(select_sql)
+        cur.execute(select_sql)
         # 判断数据库是否存在此条记录
-        res = self.cur.fetchone()[0] != ''
+        res = cur.fetchone()[0] == 0
+        # 关闭查询
+        cur.close()
+        
+        # 关闭数据库连接
+        conn.close()      
         return res
 
     # 从数据库获取一条未被爬取过的url
     def getUrlByNotCraw(self):
+        conn = pymysql.connect(host='39.106.154.2', user='root', password='root', database='maven')
+        cur = conn.cursor()
         # 查询数据库是否存在当条数据
         select_sql = 'SELECT url FROM bokeyuan_url_list WHERE is_craw = 0 limit 1'
         # 执行查询sql查询数据库是否存在未爬取数据
-        self.cur.execute(select_sql)
-        res = self.cur.fetchone()[0]
+        cur.execute(select_sql)
+        res = cur.fetchone()[0]
+        # 关闭查询
+        cur.close()
+        
+        # 关闭数据库连接
+        conn.close()      
         # 如何数据库不存在未爬取的url我们将返回no
         if res == '':
             return 'no'
@@ -67,19 +75,23 @@ class Database(object):
 
     # 更新爬取过的url的状态为1(已爬取)
     def updateUrl(self, url):
+        conn = pymysql.connect(host='39.106.154.2', user='root', password='root', database='maven')
+        cur = conn.cursor()
         # 更新sql语句
-        update_sql = 'UPDATE bokeyuan_url_list SET is_craw = 1 WHERE url = %s' % (
+        update_sql = "UPDATE bokeyuan_url_list SET is_craw = 1 WHERE url = '%s'" % (
             url)
         try:
-            self.cur.execute(update_sql)
+            cur.execute(update_sql)
             # 事务提交
-            self.conn.commit()
+            conn.commit()
             print(url + ' --------状态已更新为已爬取状态')
         except Exception as e:
             print('-------更新异常 : ' + e)
             # 事务回滚
-            self.conn.rollback()
-# if __name__ == '__main__':
-#         data = Database()
-#         url = data.getUrlByNotCraw()
-#         print(url)
+            conn.rollback()
+        # 关闭查询
+        cur.close()
+        
+        # 关闭数据库连接
+        conn.close()      
+
--- python_spider/bokeyuan_spider/spider_main.py
+++ python_spider/bokeyuan_spider/spider_main.py
@@ -23,34 +23,25 @@ class SpiderMain(object):
 		# 数据库url管理器器
 		self.database = database.Database()
 	def craw(self, root_url, url_type):
-		# 爬取网页数量控制变量
-		count = 1;
-		
-		# 判断是否存在未爬取的新的url
-# 		while self.database.getUrlByNotCraw() != 'no':
 		try:
-				# 获取一个未爬取的url
-# 				new_url = self.database.getUrlByNotCraw()
-				print('craw %d : %s' % (count, root_url))
-				# 使用html页面下载器下载页面数据
-				html_data = self.downloader.downloade(root_url)
-				# 分析获取爬取页面的我们需要url与数据
-				new_urls,new_data = self.parser.parse(root_url, html_data)
-				# 输出我们爬取的页面数据
-				self.outputer.collect_data(new_data)
-				print('urls:' + new_urls)
-# 				if count == 10000:
-# 					break
-# 				count = count + 1
+			print('CRAW URL: %s' % (root_url))
+			# 使用html页面下载器下载页面数据
+			html_data = self.downloader.downloade(root_url)
+			# 分析获取爬取页面的我们需要url与数据
+			new_urls = self.parser.parse(html_data)
+			
+			for url in new_urls :
+				self.database.insertUrl(url, url_type, '')
+			
 			
 		except :
 				print('craw falied !!!')
 
 if __name__ == '__main__':
-		print('-------------' + 'craw start' + '-------------')
-		for page in range(1, 21):
-			root_url = 'https://www.cnblogs.com/cate/java/#p%d' % (page)
-			url_type = 'java'
-			obj_spider = SpiderMain()
+		print('-------------craw start-------------')
+		url_type = 'java'
+		obj_spider = SpiderMain()
+		for page in range(1, 201):
+			root_url = 'https://www.cnblogs.com/cate/java/%d' % (page)
 			obj_spider.craw(root_url, url_type)
-		print('-------------' + 'craw end' + '-------------')
+		print('-------------craw end-------------')
