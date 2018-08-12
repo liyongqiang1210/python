89c45147ef0141cb122e2fcac5f18ef897a9b0f4
liyongqiang <1240965061@qq.com>
2018-08-12 21:05:43 +0800

    更新爬虫

--- python_spider/toutiao/CnblogsSpider.py
+++ python_spider/toutiao/CnblogsSpider.py
@@ -15,13 +15,12 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-class Spider(object):
+class CnblogsSpider(object):
 
-    # 定义文章类型列表集合
+    # 定义博客类型列表集合
     NEWS_TYPE_LIST = {
-        'news_tech','news_entertainment','news_game','news_sports',
-        'news_car','news_finance','funny','news_military','news_fashion','news_discovery',
-        'news_history','news_world','news_travel','news_baby','news_essay','news_food'
+        'java','cpp','php','python','ruby','c','go','r','108702','design','dp','javascript','jquery','html5',
+        'android','ios','sqlserver','oracle','mysql','nosql','bigdata','linux'
         }
 
     # 打开driver方法
@@ -33,75 +32,71 @@ class Spider(object):
         chrome_options.add_argument('--disable-gpu')
         driver_path = 'E:\python\Scripts\chromedriver.exe' #本地chormedriver.exe文件目录
         driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=driver_path)
-        # 设置最长的超时时间
-        # driver.set_page_load_timeout(10)
         
     # 关闭driver方法
     def close_driver(self):
         driver.quit()
 
-    # 下载html动态页面方法
+    # 下载html静态页面方法
     # 需要传入要下载页面的url
-    def html_downloader_dynamic(self,url):
-        if url is None:
+    def html_downloader_static(self,type):
+        if type is None:
             print('url参数为None')
             return None
-        # 浏览器打开url
-        driver.get(url)
-        # 逐渐滚动浏览器窗口,使ajax逐渐加载
+        # 分页获取
         for i in range(1, 10):
             # 将页面滚动条向下滚动
-            js = 'var q=document.documentElement.scrollTop=' + str(1000 * i)
-            # 执行js
-            driver.execute_script(js)
-            # 等待1s
-            time.sleep(2)
-        # 截屏方法
-        # driver.save_screenshot('E:/original.jpg')
-        # 获取页面源码
+            url = 'https://www.cnblogs.com/cate/' + type + '/#p' + str(i)
+            # 浏览器打开url
+            driver.get(url)
         html = driver.page_source
         return html
 
 
     # 解析html页面方法
-    def html_parser_dynamic(self, html):
+    def html_parser_static(self, html):
         if html is None:
             print('html参数为None')
             return
-        #  声明一个新闻对象列表对象
+        #  声明一个博客对象列表对象
         news_object_list = []
         # 将html转换成BeautifulSoup对象
         soup = BeautifulSoup(html, 'html.parser')
         # 筛选出我们需要的内容
-        div_list = soup.find_all('div',class_='item-inner y-box')
+        div_list = soup.find_all('div',class_='post_item')
         # 遍历筛选出的内容
         for div in div_list:
             try:
-                # 获取文章title
-                a = div.find_all('a', class_='link title')
+                # 获取博客title
+                a = div.find_all('a', class_='titlelnk')
                 if len(a) > 0:
                     title = a[0].string
-                    # 获取文章url
-                    th = a[0].get('href')
-                    title_href = th[6:len(th)]
+                    # 获取博客url
+                    title_href = a[0].get('href')
+                    
                 else:
                     continue
-                # 获取文章source
-                b = div.find_all('a', class_='lbtn source')
+                # 获取博客部分内容
+                b = div.find_all('a', class_='post_item_summary')
                 if len(b) > 0:
-                    source = (b[0].string)[0:len((b[0].string))-1]
-                    # 获取文章来源url
-                    source_href = b[0].get('href')
+                    content = b[0].string
                 else:
                     continue
-                # 获取文章标题图片url
-                c = div.find_all('img')
+                # 获取博客作者和url
+                c = div.find_all('lightblue')
                 if len(c) > 0:
-                    img_url = c[0].get('src')
+                    author_name = c[0].string
+                    author_url = c[0].get('href')
                 else:
                     continue
-                # 创建文章字符串对象
-                news_object = "{'title':'" + title + "','title_href':'" + title_href + "','source':'" + source + "','img_url':'" + img_url + "'}"
+                # 获取发布时间
+                d = div.find_all('div', class_='post_item_foot')
+                if len(d) > 0:
+                    release_time = d[0].string
+                else:
+                    continue
+                # 创建博客字符串对象
+                news_object = "{'title':'" + title + "','title_href':'" + title_href + "','content':'" + content + "','author_name':'" + author_name + "'}"
                 news_object_list.append(news_object)
             except Exception as e:
                 print('异常：' + e)
@@ -112,11 +107,11 @@ class Spider(object):
     def main(self):
         try:
             self.open_driver()
-            for news_type in Spider.NEWS_TYPE_LIST:
+            for news_type in CnblogsSpider.NEWS_TYPE_LIST:
                 print('=======================>' + news_type + '爬虫启动')
-                html = self.html_downloader_dynamic('https://www.toutiao.com/ch/' + news_type + '/')
-                news_object_list = self.html_parser_dynamic(html)
-                if news_object_list != "" && news_object_list is not None:
+                html = self.html_downloader_static(news_type)
+                news_object_list = self.html_parser_static(html)
+                if news_object_list != "":
                     print(news_object_list)
                 print('=======================>' + news_type + '爬虫结束')
         except Exception as e:
@@ -126,7 +121,7 @@ class Spider(object):
         
         
 if __name__ == '__main__':
-    spider = Spider()
+    spider = CnblogsSpider()
     spider.main()
 
 
