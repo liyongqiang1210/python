5cc74f1628afba8e5a25dcbf6f7b889212546152
liyongqiang <1240965061@qq.com>
2018-10-01 19:45:19 +0800

    ..

--- python_spider/news_spider/test.py
+++ python_spider/news_spider/test.py
@@ -1,14 +1,37 @@
-import schedule
+#!/usr/bin/python3.6
+# coding=utf-8
+
 import time
 import datetime
+import random
 
 def job():
 	print(time.asctime(time.localtime(time.time())))
 
 if __name__ == '__main__':
 	
-	print(datetime.datetime.now().year)
-	schedule.every(1).minutes.do(job)
+	# print(datetime.datetime.now().year)
+	# schedule.every(1).minutes.do(job)
 
-	while True:
-		schedule.run_pending()
+	# while True:
+	# 	schedule.run_pending()
+	# print(random.randint(1,9) * 0.1) # 随机数
+
+	# 写入文件操作
+	file_name = 'cookies.json'
+	# my_open = open(file_name, 'w')
+	# #打开fie_name2路径下的my_write.txt文件,采用写入模式
+	# #若文件不存在,创建，若存在，清空并写入
+	# my_open.write('one\ntwo\n')
+	# #在文件中写入一个字符串
+	# my_open.write('three\n')
+	# my_open.close()
+
+	##检查是否正确写入
+	my_open = open(file_name, 'r')
+	#读取file_name2路径下的my_write.txt文件
+	my_infor = my_open.readlines()
+	my_open.close()
+	print(my_infor)
+
+	
--- python_spider/news_spider/weibo_spider.py
+++ python_spider/news_spider/weibo_spider.py
@@ -8,13 +8,12 @@
     新浪微博爬虫
 """
 import time
+import random
+import json
 from spider import Spider
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
 
 
 class WeiBoSpider(Spider):
@@ -27,34 +26,102 @@ class WeiBoSpider(Spider):
         print(html)
         self.close_driver()
 
-    def get_cookie_from_weibo(self, username, password):
-
-        driver_path = 'E:\Program Files (x86)\python\Scripts\chromedriver.exe'
+    # 获取cookies保存到指定文件中
+    def get_cookies(self, username, password):
+        # 打开浏览器
+        driver_path = 'E:\python\Scripts\chromedriver.exe'
         driver = webdriver.Chrome(executable_path=driver_path)
         driver.get('https://weibo.com/')
         driver.maximize_window()#将浏览器最大化
         time.sleep(10) # 等待10s等待浏览器加载页面
+
         username_elem = driver.find_element_by_name('username') # 根据name属性找到用户名输入框
         username_elem.send_keys(username) # 填写用户名
         password_elem = driver.find_element_by_name('password')
         password_elem.send_keys(password) # 填写密码
-        login = driver.find_element_by_class_name("login_btn") # 根据class属性找到登录按钮
+        login = driver.find_element_by_class_name('login_btn') # 根据class属性找到登录按钮
         login.click() # 点击登录按钮
-        time.sleep(2)
-        # 逐渐滚动浏览器窗口,使ajax逐渐加载
-        for i in range(1, 30):
-            # 将页面滚动条向下滚动
-            js = 'var q=document.documentElement.scrollTop=' + str(1000 * i)
-            # 执行js
-            driver.execute_script(js)
-            # 等待1s
-            time.sleep(1)
+        time.sleep(1)
 
-        # print(driver.page_source)
+        # 获取登录后的cookies
+        cookies = driver.get_cookies()
+        json_cookies = json.dumps(cookies)
+        # 登录完成后，将cookie保存到本地文件
+        with open('cookies.json', 'w') as f:
+            f.truncate() # 清空文件中内容
+            f.write(json_cookies)
+
         driver.quit()
+
+    def get_weibo_html(self):
+
+        driver_path = 'E:\python\Scripts\chromedriver.exe'
+        driver = webdriver.Chrome(executable_path=driver_path)
+        driver.get('https://baidu.com/') # 随便打开一个网页
+        time.sleep(2) # 等待2s等待浏览器加载页面
+
+        # 删除第一次建立连接时的cookie
+        driver.delete_all_cookies()
+        # 读取登录时存储到本地的cookie
+        with open('cookies.json', 'r', encoding='utf-8') as f:
+            cookies_list = json.loads(f.read())
+        for cookies in cookies_list:
+            driver.add_cookie({
+                'domain': '.weibo.com',  # 此处xxx.com前，需要带点
+                'name': cookies['name'],
+                'value': cookies['value'],
+                'path': '/',
+                'expires': None
+            })
+
+        # 请求登录后的微博页面
+        driver.get('https://weibo.com/u/5379876310/home?wvr=5&lf=reg')
+        # 逐渐滚动浏览器窗口,使ajax逐渐加载
+        for i in range(1, 100):
+            try:
+                # 查看更多按钮
+                more_click = driver.find_element_by_class_name('WB_cardmore WB_cardmore_noborder clearfix')
+                more_click.click()
+                # 将页面滚动条向下滚动900-1100的随机长度
+                js = 'var q=document.documentElement.scrollTop=' + str(random.randint(900,1100) * i)
+                # 执行js
+                driver.execute_script(js)
+                # 随机等待0.1-2s
+                time.sleep(random.randint(0, 9) * 0.3)
+            except Exception as e:
+                # 将页面滚动条向下滚动900-1100的随机长度
+                js = 'var q=document.documentElement.scrollTop=' + str(random.randint(900,1100) * i)
+                # 执行js
+                driver.execute_script(js)
+                # 随机等待0.1-2s
+                time.sleep(random.randint(0, 9) * 0.3)
+            
+            print(i)
+
+        time.sleep(60)
+        # 获取html页面内容
+        html = driver.page_source
+
+        driver.quit() # 退出浏览器
+        return html
+
+    # 解析html页面
+    def parser_html(self, html):
+
+        soup = BeautifulSoup(html, 'html.parser')
+        div_list = soup.find_all('div', class_='WB_feed_detail') # 微博列表
+        for div in div_list:
+            print(div)
+            # wb_nick_name = div.find('a', class_='W_f14 W_fb S_txt1') # 微博昵称
+            # wb_content = div.find('div', class_='WB_text W_f14') # 微博内容
+
+            
 
 
 
 if __name__ == '__main__':
     weibospider = WeiBoSpider()
-    cookie = weibospider.get_cookie_from_weibo('18330902178', 'L18330902178')
+    # weibospider.get_cookies('18330902178', 'L18330902178')
+    html = weibospider.get_weibo_html()
+    # weibospider.parser_html(html)
+
